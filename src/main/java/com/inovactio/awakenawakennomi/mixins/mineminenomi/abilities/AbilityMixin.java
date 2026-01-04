b284b3d4d7d4536cea90d287a841410b40fbfddc
Kévin Roy <kevin-roy@hotmail.fr>
2026-01-04 20:25:46 +0100

    feat: rename Reworked Bigan ability to Kirimanjaro and update related references

--- src/main/java/com/inovactio/awakenawakennomi/mixins/mineminenomi/abilities/AbilityMixin.java
+++ src/main/java/com/inovactio/awakenawakennomi/mixins/mineminenomi/abilities/AbilityMixin.java
@@ -2,19 +2,15 @@ package com.inovactio.awakenawakennomi.mixins.mineminenomi.abilities;
 
 import com.inovactio.awakenawakennomi.init.ModMorphs;
 import com.inovactio.awakenawakennomi.util.MorphRequirementRewritterHelper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.pixelatedw.mineminenomi.abilities.mogu.MoguraBananaAbility;
 import xyz.pixelatedw.mineminenomi.abilities.mogu.MoguraTonpoAbility;
+import xyz.pixelatedw.mineminenomi.abilities.ushigiraffe.BiganAbility;
 import xyz.pixelatedw.mineminenomi.api.abilities.Ability;
-import xyz.pixelatedw.mineminenomi.api.abilities.AbilityCore;
 import xyz.pixelatedw.mineminenomi.api.abilities.components.AbilityComponent;
-import xyz.pixelatedw.mineminenomi.api.abilities.components.RequireMorphComponent;
-import xyz.pixelatedw.mineminenomi.api.morph.MorphInfo;
 
 @Mixin(Ability.class)
 public class AbilityMixin {
@@ -37,6 +33,14 @@ public class AbilityMixin {
                 MoguraBananaAbility.class,
                 ModMorphs.AWAKEN_MOGU.get(),
                 xyz.pixelatedw.mineminenomi.init.ModMorphs.MOGU_HEAVY.get()
+        );
+        MorphRequirementRewritterHelper.replaceRequireMorphIfInstance(
+                this,
+                components,
+                BiganAbility.class,
+                ModMorphs.AWAKEN_GIRAFFE.get(),
+                xyz.pixelatedw.mineminenomi.init.ModMorphs.GIRAFFE_WALK.get(),
+                xyz.pixelatedw.mineminenomi.init.ModMorphs.GIRAFFE_HEAVY.get()
         );
     }
 }
